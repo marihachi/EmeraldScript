8e94dd50a8291de61fda5505c73e2a8d1d0e2b2d
marihachi <marihachi0620@gmail.com>
2019-06-02 17:43:28 +0900

    fix

--- src/cli/commandUtil.ts
+++ src/cli/commandUtil.ts
@@ -11,7 +11,7 @@ export function showHelp(command?: string)
 	if (!command) {
 		console.log('Usage: em <command>\n');
 		console.log('where <command> is one of:');
-		console.log('    build, init, login, publish\n');
+		console.log('    build, login, publish\n');
 		console.log('em <command> -h  command help');
 		return;
 	}
