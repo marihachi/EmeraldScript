6c8423ef04d305953aae9da8dec2a5b3f5946755
marihachi <marihachi0620@gmail.com>
2020-01-11 18:58:18 +0900

    support version compairing of Misskey instance

--- src/cli/commands/login.ts
+++ src/cli/commands/login.ts
@@ -4,10 +4,12 @@ import { promisify } from 'util';
 import path from 'path';
 import Config from '../misc/Config';
 import inputLine from '../misc/inputLine';
+import semver from 'semver';
 
 import {
 	App,
-	AuthSession
+	AuthSession,
+	Configuration
 } from 'corekey';
 
 const mkdir = promisify(fs.mkdir);
@@ -24,7 +26,36 @@ export default async function(args: string[])
 	let host = await inputLine('Input the host name of misskey instance (default: misskey.io) > ');
 	if (host == '') host = 'misskey.io';
 
-	const app = await App.create(host, 'Emerald Script', 'A text based script language for Misskey Pages', ['read:pages', 'write:pages']);
+	const versionRes: Record<string, any> = await Configuration.Requester.request(host, 'version', { }, false);
+	console.log('instance version:', versionRes.version);
+	const parsed = semver.parse(versionRes.version);
+	if (!parsed) {
+		throw `unknown instance version`;
+	}
+	const versionStr: string = versionRes.version;
+	
+	let permissions: string[];
+
+	const m544Instance = parsed.prerelease.length == 1 && parsed.prerelease[0] == 'm544';
+
+	if (m544Instance) {
+		if (semver.gte(versionStr, '10.102.111')) {
+			permissions = ['account-read', 'note-write'];
+		}
+		else {
+			throw `This instance does not support Misskey Pages`;
+		}
+	}
+	else {
+		if (semver.gte(versionStr, '11.5.0')) {
+			permissions = ['read:pages', 'write:pages'];
+		}
+		else {
+			throw `This instance does not support Misskey Pages`;
+		}
+	}
+
+	const app = await App.create(host, 'Emerald Script', 'A text based script language for Misskey Pages', permissions);
 	const session = await AuthSession.generate(app);
 	console.log('start login:', session.url);
 	const account = await session.waitForAuth();
